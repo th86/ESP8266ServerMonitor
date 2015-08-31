27bfb0cd2ed756acf0a4db5c3485d3b9550f9223
th86 <th8623@gmail.com>
2015-08-31 11:07:06 -0400

    IP  storage and making sure poke will run
    fix the IP display and repeatedly poking the server.

--- web.lua
+++ web.lua
@@ -3,11 +3,9 @@ wifi.setmode(wifi.STATION)
 wifi.sta.config("MY_SSID","MY_PASSWORD")
 LED_1=5; --GPIO14
 LED_2=6; --GPIO12
-is_on=0; --GPIO16
-gpio.mode(LED_1, gpio.OUTPUT);gpio.mode(LED_2, gpio.OUTPUT);gpio.mode(is_on, gpio.INPUT);
+gpio.mode(LED_1, gpio.OUTPUT);gpio.mode(LED_2, gpio.OUTPUT);
 gpio.write(LED_1, gpio.LOW);gpio.write(LED_2, gpio.LOW);
 require("settings");
-print(IP_record);
 function setting_HTTP()
     print(wifi.sta.getip());
     print("Setting mode");
@@ -26,10 +24,11 @@ function setting_HTTP()
         buf = buf.."<input type=\"submit\" value=\"Submit\">";
         buf = buf.."<p>Tai-Hsien Ouyang, 2015<\p></form></body></html>";
         if parsePost[2]~=nil then
-            IP_record='IP_record='..'"'..string.sub(request,parsePost[2]+1,#request)..'"';
-            print(IP_record);
+            IP_record_print='IP_record='..'"'..string.sub(request,parsePost[2]+1,#request)..'"';
+            IP_record=string.sub(request,parsePost[2]+1,#request)
+            print(IP_record_print);
             file.open("settings.lua","w+");
-            file.writeline(IP_record);
+            file.writeline(IP_record_print);
             file.close();
         end
         client:send(buf);
@@ -37,8 +36,12 @@ function setting_HTTP()
         collectgarbage();
         end)
     end)
+    tmr.alarm(0,29000,0,function() print("Closing settings server..."); srv:close(); end);
 end
-function poke_server(server_IP)
+function poke_server()
+    print("poking mode:"..IP_record);
+    gpio.write(LED_1, gpio.LOW);
+    gpio.write(LED_2, gpio.LOW);
     poker= nil;
     poker=net.createConnection(net.TCP, 0);
     poker:on("receive", function(poker, payload)
@@ -61,10 +64,9 @@ function poke_server(server_IP)
         poker:close();
         collectgarbage();
     end)
-    poker:connect(80,server_IP);
-    is_on=tonumber(gpio.read(is_on));
+    poker:connect(80, IP_record);
 end
 
-tmr.alarm(0,1000,0,function() setting_HTTP();end);
-tmr.alarm(1,60000,0,function(IP_record) poke_server(IP_record) end); --60sec
-
+setting_HTTP();
+print(IP_record);
+tmr.alarm(1,35000,1,function() poke_server() end);
